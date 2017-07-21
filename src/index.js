9d6ba12d49c2e5748c671dff8e4cb96e838554cc
Daniel Parsons <danielparsons3133382@gmail.com>
2017-07-21 19:24:51 +1000

    Using State to create an interactive component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-var Component = function(props) {
-  return (
-    <div style={{color: props.color}}>
-      <h1>{props.greeting}</h1>
-    </div>
-  )
-}
+var Component = React.createClass({
+  getInitialState: function() {
+    return{
+      color: 'blue'
+    };
+  },
+
+  handleButtonClick: function() {
+    this.setState(function (prevState) {
+      return {
+        color: (prevState.color === 'blue' ? 'green' : 'blue')
+      }
+    })
+  },
+
+  render: function() {
+    console.log(this.state.color)
+    return(
+      <div>
+        <div style={{color: this.state.color}}>
+          <h1>{this.props.greeting}</h1>
+        </div>
+        <button onClick={this.handleButtonClick}>Click Me</button>
+      </div>
+    )
+  }
+})
 
 ReactDOM.render(
-  <div>
-    <Component greeting="Hey you!" color="blue" />,
-    <Component greeting="Out there on your own." color="green" />
-  </div>,
+  <Component greeting="Hello World"  />,
   document.getElementById('root')
 )
