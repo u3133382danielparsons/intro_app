d70d3a405d14738da66c09bf9c93ef658adfb68b
Daniel Parsons <danielparsons3133382@gmail.com>
2017-07-21 18:11:49 +1000

    Hello World with jsx

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+var React = require('react')
+var ReactDOM = require('react-dom')
 
-var App = React.createElement('h1',null, 'Hello World');
+var App = <h1>Hello World</h1>
 
 ReactDOM.render(
   App,
   document.getElementById('root')
-);
+)
