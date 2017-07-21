73a9deb36ac175b2856e78b40960133e36c88948
Daniel Parsons <danielparsons3133382@gmail.com>
2017-07-21 18:08:39 +1000

    Hello World

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createElement('h1',null, 'Hello World');
+
+ReactDOM.render(
+  App,
+  document.getElementById('root')
+);
