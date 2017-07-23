9c349f24bded7212562cddddbfcea86e135af1bc
Daniel Parsons <danielparsons3133382@gmail.com>
2017-07-23 19:13:07 +1000

    ES6 syntax introduction

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
-var shows = require('./shows.json')
-var Show = require('./Show')
+// var React = require('react')
+// var ReactDOM = require('react-dom')
+// var shows = require('./shows.json')
+// var Show = require('./Show')
+//
+//
+// ReactDOM.render(
+//   <Show shows={shows} />,
+//   document.getElementById('root')
+// )
+/* Function Constructor */
+var Employee = function(firstName, lastName) {
+  this.firstName = firstName
+  this.lastName = lastName
 
-ReactDOM.render(
-  <Show shows={shows} showIndex={2} />,
-  document.getElementById('root')
-)
+}
+
+
+Employee.prototype.getFullName = function() {
+  return this.firstName + ' ' + this.lastName
+}
+var sam = new Employee('Sam', 'Williams')
+// console.log(sam)
+// console.log(sam.getFullName())
+console.log(Employee.prototype === sam.__proto__)
+
+
+/* ES6 Class */
+class EmployeeES6 {
+  constructor(firstName,lastName) {
+    // console.log('Inside EmployeeES6')
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+
+  getFullName(){
+    return this.firstName + ' ' + this.lastName
+  }
+}
+
+var sammy = new EmployeeES6('Sammy', 'Winchester')
+// console.log(sammy)
+// console.log(sammy.getFullName())
+console.log(EmployeeES6.prototype === sammy.__proto__)
